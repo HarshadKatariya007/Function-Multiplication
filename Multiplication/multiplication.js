d12a2c7a0a352117b66d1a9f921c251b72729648
Harshad Katariya <159020392+HarshadKatariya007@users.noreply.github.com>
2024-02-23 15:36:13 +0530

    Add files via upload

---
+++ Multiplication/multiplication.js
@@ -0,0 +1,12 @@
+function multiplication() 
+{
+    let one=1;
+    let two=2;
+    let three=3;
+    let four=4;
+    let five=5;
+    let multi=one*two*three*four*five;
+
+    return multi;
+}
+console.log(multiplication());
